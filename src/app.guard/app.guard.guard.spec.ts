03d096a5f5252907a4e720fa964b167b824e593f
Noah <rxueqiang@163.com>
2023-10-24 16:08:44 +0800

    feat: add guard

---
+++ src/app.guard/app.guard.guard.spec.ts
@@ -0,0 +1,7 @@
+import { AppGuardGuard } from './app.guard.guard';
+
+describe('AppGuardGuard', () => {
+  it('should be defined', () => {
+    expect(new AppGuardGuard()).toBeDefined();
+  });
+});
